3b7895b56f1400dd3b4ea2d81a92d1a2c9fd950e
Arya Maulana deryawan <111832631+Aryamd@users.noreply.github.com>
2023-12-02 20:23:43 +0700

    Create bank.java

---
+++ bank.java
@@ -0,0 +1,35 @@
+package pa_pbo;
+
+import java.util.ArrayList;
+
+public class Bank {
+    private ArrayList<Nasabah> nasabahList;
+    
+    public Bank() {
+        this.nasabahList = new ArrayList<>();
+    }
+    
+    public void tambahNasabah(Nasabah nasabah) {
+        nasabahList.add(nasabah);
+    }
+    
+    public ArrayList<Nasabah> getNasabahList() {
+        return nasabahList;
+    }
+    
+    public void transfer(Nasabah pengirim, Nasabah penerima, double jumlah) {
+        if (pengirim.getNama().equals(penerima.getNama())) {
+            System.out.println("Transfer gagal. Nasabah pengiirm dan penerima sama.");
+            return;
+        }
+        
+        if (pengirim.rekening.tarikTunai(jumlah)) {
+            penerima.rekening.tambahSaldo(jumlah);
+            pengirim.tambahTransaksi(new Transaksi("transfer", jumlah, penerima.getNama()));
+            penerima.tambahTransaksi(new Transaksi("terima transfer",jumlah, pengirim.getNama()));
+            System.out.println("Transfer berhasil.");
+        } else {
+            System.out.println("Transfer gagal. Saldo tidak mencukupi");
+        }
+    }
+}
