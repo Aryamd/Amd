0e0aa3db7d4746b9de36a8fdc71f7e3a4f64a084
Arya Maulana deryawan <111832631+Aryamd@users.noreply.github.com>
2023-12-02 21:39:25 +0700

    Create nasabahutama.java

---
+++ nasabahutama.java
@@ -0,0 +1,12 @@
+package pa_pbo;
+
+public class NasabahPrioritas extends User{
+    public NasabahPrioritas(Nasabah nasabah) {
+        super(nasabah);
+    }
+    
+    //tambah fitur sendiri++
+    public void tambahFiturSendiri() {
+        System.out.println("Fitur tambahan dari Nasabah Priorias");
+    }
+}
