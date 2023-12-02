c34dc35e32b7da37949c0a6107f57f3635b9327b
Arya Maulana deryawan <111832631+Aryamd@users.noreply.github.com>
2023-12-02 21:44:19 +0700

    Create Teller.java

---
+++ Teller.java
@@ -0,0 +1,13 @@
+package pa_pbo;
+
+class Teller extends Admin {
+    public Teller(Bank bank) {
+        super(bank);
+    }
+    
+    public void prosesSetoran(Nasabah nasabah, double jumlah) {
+        nasabah.rekening.tambahSaldo(jumlah);
+        nasabah.getRekening().tambahSaldo(jumlah);
+        System.out.println("Setoran berhasil.");
+    }
+}
